732d88d1e4e4716832d8b0788f00f482b67198fd
Ritik232 <rmittal@calance.com>
2023-03-28 15:01:33 +0530

    added pmd

--- hello_world.py
+++ hello_world.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request
   
 app = Flask(__name__)
   
-  
 @app.route('/hello', methods=['GET'])
 def helloworld():
-    if(request.method == 'GET'):
-        data = {"data": "Hello World I am ritik"}
-        return jsonify(data)
-  
+    # if(request.method == 'GET'):
+    data = {
+        "data": "Hello World I am ritik"
+    }
+    return jsonify(data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=9000)
